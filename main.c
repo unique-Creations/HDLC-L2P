524ed1e14e5bdf1bbdb661ec483445e318f26aff
unique-Creations <30441137+unique-Creations@users.noreply.github.com>
2021-09-27 21:17:16 -0700

    Added some code to begin with main.c

---
+++ main.c
@@ -0,0 +1,21 @@
+//
+// @author Ernesto Ruiz
+//
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+enum flag {b7 = 0, b6 = 1, b5 = 1, b4 = 1, b3 = 1, b2 = 1, b1 = 1, b0 = 0};
+/**
+ *
+ * @param c
+ * @return boolean value of whether flag has been
+ */
+bool check_flag(char c);
+int main(){
+
+    char tmp;
+    tmp = getc(stdin);
+    printf("%d", b4);
+    return 0;
+}
