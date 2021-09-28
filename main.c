56ec06d3fd71d648b4109fcee87f9586e8b4cecc
unique-Creations <30441137+unique-Creations@users.noreply.github.com>
2021-09-27 21:37:09 -0700

    Added function that is to detect the flag. Also, boolean variable to store flag state.

--- main.c
+++ main.c
@@ -5,17 +5,31 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+/* flag enum to detect*/
 enum flag {b7 = 0, b6 = 1, b5 = 1, b4 = 1, b3 = 1, b2 = 1, b1 = 1, b0 = 0};
+
 /**
  *
- * @param c
+ * @param c character user inputs
+ * @param f boolean flag state
  * @return boolean value of whether flag has been
  */
-bool check_flag(char c);
+void detect_flag(char c, bool *f);
+
 int main(){
+    bool flag_detected = false;
+    printf("Enter any key: ");
 
     char tmp;
     tmp = getc(stdin);
-    printf("%d", b4);
+    detect_flag(tmp, &flag_detected);
+    if (flag_detected){
+        putchar(tmp);
+    }
+
     return 0;
+}
+
+void detect_flag(char c, bool *f){
+
 }
