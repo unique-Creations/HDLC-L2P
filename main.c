8212dce2828ffaab989c565004cdabefa9c745e0
m1macER <eruiz6@mail.sfsu.edu>
2021-09-28 15:44:38 -0700

    HDLC almost complete.

--- main.c
+++ main.c
@@ -4,32 +4,70 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 
-/* flag enum to detect*/
-enum flag {b7 = 0, b6 = 1, b5 = 1, b4 = 1, b3 = 1, b2 = 1, b1 = 1, b0 = 0};
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
+
+/* Initialize global variables */
+int user_bits[8];
+int user_bit_size;
+
+/* Integer array of HDLC flag
+ * flag: 01111110
+ * */
+const int flag[] = {0, 1, 1, 1, 1, 1, 1, 0};
 
 /**
- *
- * @param c character user inputs
  * @param f boolean flag state
+ * @brief check whether flag has been received from user.
  * @return boolean value of whether flag has been
  */
-void detect_flag(char c, bool *f);
+void detect_flag(bool *f);
 
-int main(){
+/**
+ * @param c user character that is to be converted to int.
+ *  @brief Adds bit to user_bits array if in correct order.
+ *         Increase user_bit_size if bit is added to array.
+ */
+void append_flag_bit(char c);
+
+int main() {
     bool flag_detected = false;
-    printf("Enter any key: ");
+    printf("Enter any key:\n");
 
-    char tmp;
-    tmp = getc(stdin);
-    detect_flag(tmp, &flag_detected);
-    if (flag_detected){
-        putchar(tmp);
+    //TODO: if user enters a wrong key between flag input, start over.
+    while (1) {
+        char tmp;
+        tmp = (char) getc(stdin);
+        detect_flag(&flag_detected);
+        if (tmp == '0' || tmp == '1') {
+            append_flag_bit(tmp);
+        }
+        // If HDLC flag is detected, print input.
+        if (flag_detected) {
+            putchar(tmp);
+        }
     }
-
-    return 0;
 }
 
-void detect_flag(char c, bool *f){
-
+void detect_flag(bool *f) {
+    // Check if flag has been received from user.
+    if (user_bit_size == 8) {
+        *f = !*f;
+        user_bit_size = 0;
+        printf("\nFlag has been detected\n");
+    }
 }
+
+void append_flag_bit(char c) {
+    int tmp = c - '0';
+    if (tmp == flag[user_bit_size]) {
+        user_bits[user_bit_size] = tmp;
+        user_bit_size += 1;
+    } else {
+        user_bit_size = 0;
+    }
+}
+
+#pragma clang diagnostic pop
